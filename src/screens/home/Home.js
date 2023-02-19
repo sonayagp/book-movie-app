c34d3be8bc07657c05892a4acd81d46e322137ea
sonukrr <sonuec12@gmail.com>
2023-02-19 16:39:51 +0530

    sonu_adding-header

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import Header from '../../common/header/Header';
 
 export default class Home extends Component {
+
+
+
   render() {
-    return <h2>Home</h2>;
+    return (
+      <div>
+        <Header baseUrl={this.props.baseUrl} />
+
+      </div>
+    );
   }
+
 }
